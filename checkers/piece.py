14bb7a7d1f8facb73cbc8ab54231cb66f70dcc79
laote <ihagiu1212@gmail.com>
2024-11-19 17:06:55 +0200

    starting to implement the move of the figures on the board in board and piece.py i created 2 methods for the move functionality

--- checkers/piece.py
+++ checkers/piece.py
@@ -12,7 +12,7 @@ class Piece:
         self.row = row
         self.col = col
         self.color = color
-        self.king = True
+        self.king = False
 
         if self.color == RED:
             self.direction = -1
@@ -39,6 +39,12 @@ class Piece:
         pygame.draw.circle(win, self.color, (self.x, self.y), radious)
         if self.king:
             win.blit(CROWN, (self.x - CROWN.get_width()//2, self.y - CROWN.get_height()// 2))
+
+
+    def move(self, row, col):
+        self.row = row
+        self.col = col
+        self.calculate_position()
 
     def __repr__(self):
         return str(self.color)
--- main_app.py
+++ main_app.py
@@ -22,6 +22,10 @@ def main():
     clock = pygame.time.Clock()
     board = Board()
 
+
+    piece = board.get_piece(0, 1)
+    board.move(piece, 4, 3)
+
     while run:
         clock.tick(FPS)
         
