4c50aeb9c21e551502825c87a3600e2c18ab88ea
laote <ihagiu1212@gmail.com>
2024-11-19 11:27:42 +0200

    creating the main function, creating few events one if we hit the exit icon the game will close same if we press the mouse key.inside the main function i created a variable called fps that was passed in the clock event(this variable helps us to run the game on all computers at the same speed

--- main_app.py
+++ main_app.py
@@ -3,9 +3,35 @@
 # import pygame(check if it s works you should after run the file "Hello from pygame community" if you got this all good)
 import pygame
 
+FPS = 60
 
 # import the checkers file in the main file
 from checkers.constants import WIDTH, HEIGHT
 
-
+# Window and caption(name of the app)
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Checkers")
+
+
+
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(FPS)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+
+    pygame.quit()
+
+
+
+main()
