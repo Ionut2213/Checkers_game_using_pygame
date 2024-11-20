a97f252d6f638152117842974ecb6f9de9319120
laote <ihagiu1212@gmail.com>
2024-11-20 13:20:39 +0200

    in the main file we create a function thats makes the moves based on the mouse input

--- main_app.py
+++ main_app.py
@@ -6,7 +6,7 @@ import pygame
 FPS = 60
 
 # import the checkers file in the main file
-from checkers.constants import WIDTH, HEIGHT
+from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE
 from checkers.board import Board
 
 # Window and caption(name of the app)
@@ -14,7 +14,11 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Checkers")
 
 
-
+def get_row_cel_from_mouse(pos):
+    x, y = pos
+    row = y // SQUARE_SIZE
+    col = x // SQUARE_SIZE
+    return row, col
 
 
 def main():
@@ -23,8 +27,6 @@ def main():
     board = Board()
 
 
-    piece = board.get_piece(0, 1)
-    board.move(piece, 4, 3)
 
     while run:
         clock.tick(FPS)
@@ -34,7 +36,10 @@ def main():
                 run = False
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pass
+                pos = pygame.mouse.get_pos()
+                row, col = get_row_cel_from_mouse(pos)
+                piece = board.get_piece(row, col)
+                board.move(piece, 4, 3)
 
             
         board.draw(WIN)
